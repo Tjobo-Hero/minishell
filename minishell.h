8829983a766083333baced7eaeecebda93fc5e66
Tim Van citters <tvan-cit@f0r1s14.codam.nl>
2020-06-04 16:48:00 +0200

    asdf

--- minishell.h
+++ minishell.h
@@ -6,14 +6,31 @@
 /*   By: tvan-cit <tvan-cit@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/04 15:53:15 by tvan-cit      #+#    #+#                 */
-/*   Updated: 2020/06/04 16:06:54 by tvan-cit      ########   odam.nl         */
+/*   Updated: 2020/06/04 16:47:32 by tvan-cit      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <studio.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
 
+# include <stdio.h> // bij inleveren weg halen
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
 
+/*
+** Functions for get next line
+*/
+int		get_next_line(int fd, char **line);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s1);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+char	*ft_strchr(const char *s, int c);
+
+int 	main(void);
 #endif
