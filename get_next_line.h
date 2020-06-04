c3cac3ed1b5cb69b8a36571a18306b67fcc820ed
Tim Van citters <tvan-cit@f0r1s14.codam.nl>
2020-06-04 16:44:44 +0200

    change this

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: tvan-cit <tvan-cit@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/11/21 12:01:00 by tvan-cit       #+#    #+#                */
+/*   Updated: 2019/12/02 15:26:10 by tvan-cit      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+
+int		get_next_line(int fd, char **line);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s1);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+char	*ft_strchr(const char *s, int c);
+#endif
