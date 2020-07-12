5c6c9474e101798034356bc0078e601bf1dcb7ef
René Braaksma <renebraaksma@MacBook-Pro-van-Rene.local>
2020-07-12 22:14:38 +0200

    Unset

--- src/cmd_unset.c
+++ src/cmd_unset.c
@@ -6,55 +6,44 @@
 /*   By: renebraaksma <renebraaksma@student.42.f      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/07/03 14:55:19 by rbraaksm      #+#    #+#                 */
-/*   Updated: 2020/07/09 13:01:10 by rbraaksm      ########   odam.nl         */
+/*   Updated: 2020/07/11 21:37:19 by rbraaksm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// void	delete_env(t_mini *d, int i)
-// {
-// 	char	*tmp;
+void	remove_from_list(char *arg, t_env **env)
+{
+	int	i;
+	int	c;
 
-// 	while (d->env[i + 1])
-// 	{
-// 		tmp = d->env[i];
-// 		d->env[i] = d->env[i + 1];
-// 		d->env[i + 1] = tmp;
-// 		i++;
-// 	}
-// 	free(d->env[i]);
-// 	d->env[i] = NULL;
-// 	free(d->env[i + 1]);
-// 	d->c_env--;
-// }
+	i = 0;
+	c = 0;
+	while (env[c] != NULL)
+		c++;
+	while (ft_strncmp(arg, env[i]->head, ft_strlen(arg)))
+		i++;
+	while (i < c)
+	{
+		clear_list()
+	}
+}
 
 int		**unset(t_mini *d)
 {
-// 	int		i;
-// 	int		a;
-// 	int		len;
+	t_env	*tmp;
+	int		a;
 
-// 	a = 1;
-// 	len = 0;
-// 	while (d->args[a])
-// 	{
-// 		i = 0;
-// 		while (d->env[i])
-// 		{
-// 			if (ft_strchr(d->args[a], '='))
-// 				break ;
-// 			len = ft_strlen(d->args[a]);
-// 			if (ft_strncmp(d->env[i], d->args[a], len) == 0
-// 				&& (d->env[i][len] == '=' || d->env[i][len] == '\0'))
-// 			{
-// 				delete_env(d, i);
-// 				break ;
-// 			}
-// 			i++;
-// 		}
-// 		a++;
-// 	}
-(void)d;
+	a = 1;
+	while (d->args[a])
+	{
+		tmp = look_up(d->args[a], d->echo);
+		if (tmp)
+		{
+			delete_lst(d->args[a], d->echo);
+			remove_from_list(d->args[a], d->env);
+		}
+		a++;
+	}
 	return (0);
 }
